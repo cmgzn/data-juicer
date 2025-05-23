ed07f451125e9afa59fb1777bbeb719eeccc5a65
cmgzn <zdongs@outlook.com>
2025-05-23 15:46:48 +0800

    docs: update script for building multilingual docs

--- docs/sphinx_doc/build_doc.sh
+++ docs/sphinx_doc/build_doc.sh
@@ -3,6 +3,6 @@ make clean
 languages=(en zh-CN)
 
 for lang in "${languages[@]}"; do
-    sphinx-multiversion source build/$lang -D "language=$lang" --dump-metadata
+    sphinx-multiversion source build/$lang -D "language=$lang"
 done
 
