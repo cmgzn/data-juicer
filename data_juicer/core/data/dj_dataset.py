2a7d0c14f7b6a3c0920da298095f638bf71e7683
cmgzn <zdongs@outlook.com>
2025-07-23 11:23:29 +0800

    refactor: move logger import inside methods to delay loading

--- data_juicer/core/data/dj_dataset.py
+++ data_juicer/core/data/dj_dataset.py
@@ -12,7 +12,6 @@ from typing import Any, Dict, List, Optional, Union
 
 from datasets import Dataset, DatasetDict, is_caching_enabled
 from datasets.formatting.formatting import LazyBatch
-from loguru import logger
 
 from data_juicer.core.data.schema import Schema
 from data_juicer.core.monitor import Monitor
@@ -259,6 +258,7 @@ class NestedDataset(Dataset, DJDataset):
         adapter=None,
         open_monitor=True,
     ):
+        from loguru import logger
         if operators is None:
             return self
 
@@ -514,6 +514,7 @@ def nested_query(root_obj: Union[NestedDatasetDict, NestedDataset, NestedQueryDi
                 # dive into next level
                 tmp = nested_obj_factory(tmp[".".join(subkeys[i : i + 1])])
             else:
+                from loguru import logger
                 logger.debug(
                     f"cannot find item given key={key} in dataset="
                     f"{root_obj}. For the final caught outer-exception,"
